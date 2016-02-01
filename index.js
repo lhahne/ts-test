3a065930e1ea27408a203630c341185a355e593b
Lauri Hahne <lauri.hahne@gofore.com>
2016-02-01 14:32:37 +0200

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var connect  = require('connect');
-var serve = require('serve-static');
+const connect  = require('connect');
+const serve = require('serve-static');
 
-var server = connect();
+const server = connect();
 
 server.use(serve(__dirname + '/public'));
 
 server.listen(9000);
 
-var livereload = require('livereload');
+const livereload = require('livereload');
 server = livereload.createServer();
 server.watch(__dirname + "/public");
 

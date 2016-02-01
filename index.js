4f481c59bafe16b9d83c347fa3eb58956dadc9f3
Lauri Hahne <lauri.hahne@gofore.com>
2016-02-01 14:33:07 +0200

    refactor harder

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ server.use(serve(__dirname + '/public'));
 server.listen(9000);
 
 const livereload = require('livereload');
-server = livereload.createServer();
-server.watch(__dirname + "/public");
+const reload = livereload.createServer();
+reload.watch(__dirname + "/public");
 
 console.log("running on http://localhost:9000");
